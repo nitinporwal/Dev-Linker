98a52c558008119fbd357dc6a5ebaba2c5da2da2
nitin-codr <nitinporwal99@gmail.com>
2020-09-03 16:52:36 +0530

    implement jwt token for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 // @route POST api/users
 // @desc Register route
 // @access Public
@@ -33,8 +35,24 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      console.log(req.body);
-      res.send('User registered');
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      //   console.log(req.body);
+      //   res.send('User registered');
     } catch (err) {
       console.error({ error: [{ message: err.message }] });
       res.status(500).send('Server error');
