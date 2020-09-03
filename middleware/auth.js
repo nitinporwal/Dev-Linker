42bd8ad7b5fd45de9f4ea9e45f5be1861257a7ea
nitin-codr <nitinporwal99@gmail.com>
2020-09-03 18:58:54 +0530

    custom auth middleware and jwt verify

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../models/User');
+
+module.exports = function (req, res, next) {
+  //Get token
+  const token = req.header('x-auth-token');
+  if (!token) {
+    res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  try {
+    const decode = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decode;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
