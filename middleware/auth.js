7c7572f3cd01c27f705fd6a5b248630658f43a90
nitin-codr <nitinporwal99@gmail.com>
2020-09-09 01:14:06 +0530

    login route setup

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function (req, res, next) {
   }
   try {
     const decode = jwt.verify(token, config.get('jwtSecret'));
-    req.user = decode;
+    req.user = decode.user;
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
