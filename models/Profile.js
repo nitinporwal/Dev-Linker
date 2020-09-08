fb91daa52e54ece940d0a99c9c258277ddc9c49c
nitin-codr <nitinporwal99@gmail.com>
2020-09-09 01:48:52 +0530

    get current user profile route setup

--- models/Profile.js
+++ models/Profile.js
@@ -104,3 +104,5 @@ const ProfileSchema = mongoose.Schema({
     default: Date.now,
   },
 });
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['email', 'avatar']);
+
+    if (!profile) {
+      res.status(400).json({ msg: 'Proflie not found' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error({ error: [{ msg: err.message }] });
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
