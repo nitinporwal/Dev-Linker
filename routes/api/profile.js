c1743f17bb9fd15c17cd44ee00b9378aa2c626d4
nitin-codr <nitinporwal99@gmail.com>
2020-09-11 22:47:08 +0530

    add delete route for experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -167,6 +167,7 @@ router.put(
       }
       profile.experience.unshift(newExperience);
       await profile.save();
+
       res.json(profile);
     } catch (err) {
       console.log({ error: [{ message: err.message }] });
@@ -174,5 +175,19 @@ router.put(
     }
   }
 );
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error({ error: [{ msg: err.message }] });
+  }
+});
 
 module.exports = router;
